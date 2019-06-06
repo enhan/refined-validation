bac0c4c1f5db33b619e578ae6d72dff512e35b32
Emmanuel Nhan <nhanmanu@gmail.com>
2019-06-06 21:56:17 +0200

    Better style

--- src/main/scala/eu/enhan/validation/ValidationLogic.scala
+++ src/main/scala/eu/enhan/validation/ValidationLogic.scala
@@ -32,18 +32,30 @@ object ValidationLogic {
 
   type Name = And[NonEmpty, MaxSize[W.`5`.T]]
   type AlertName = String Refined Name
+
   type TAP = OpenClosed[W.`100`.T, W.`1000`.T]
   type TCP = Closed[W.`100`.T, W.`1000`.T]
+
   type TA = Int Refined TAP
   type TC = Int Refined TCP
+
+
   final case class Alert(name: AlertName, tA: TA, tC: TC)
 
 
   def validate(alertPayload: AlertPayload): ValidatedNel[AlertCreationError, Alert] = {
 
-    val nameValidation: Validated[AlertCreationError, AlertName] = Validated.fromEither(refineV[Name](alertPayload.name)).leftMap( _ => AlertCreationError.InvalidName)
-    val tAValidation: Validated[AlertCreationError, TA] = Validated.fromEither(refineV[TAP](alertPayload.tA)).leftMap( _ => AlertCreationError.InvalidTA)
-    val tCvalidation: Validated[AlertCreationError, TC] = Validated.fromEither(refineV[TCP](alertPayload.tC)).leftMap(_ => AlertCreationError.InvalidTC)
+    val nameValidation: Validated[AlertCreationError, AlertName] = Validated.fromEither(
+      refineV[Name](alertPayload.name)
+    ).leftMap( _ => AlertCreationError.InvalidName)
+
+    val tAValidation: Validated[AlertCreationError, TA] = Validated.fromEither(
+      refineV[TAP](alertPayload.tA)
+    ).leftMap( _ => AlertCreationError.InvalidTA)
+
+    val tCvalidation: Validated[AlertCreationError, TC] = Validated.fromEither(
+      refineV[TCP](alertPayload.tC)
+    ).leftMap(_ => AlertCreationError.InvalidTC)
 
     (nameValidation.toValidatedNel,
       tAValidation.toValidatedNel,
